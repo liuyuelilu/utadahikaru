c088ec35a7311a568e1125c9be2f1603151574db
wang <eskimo220@sina.com>
2017-08-26 23:28:58 +0800

    shiro sample

--- src/main/java/com/heroku/config/ShiroConfig.java
+++ src/main/java/com/heroku/config/ShiroConfig.java
@@ -37,7 +37,10 @@ public class ShiroConfig {
         // 拦截器.
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
         // 配置不会被拦截的链接 顺序判断
-        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/fragments/**", "anon");
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/js/**", "anon");
+        filterChainDefinitionMap.put("/stylesheets/**", "anon");
         filterChainDefinitionMap.put("/ajaxLogin", "anon");
 
         // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
--- src/main/java/com/heroku/controller/UserVaildContreller.java
+++ src/main/java/com/heroku/controller/UserVaildContreller.java
@@ -1,61 +1,72 @@
 package com.heroku.controller;
 
-import com.heroku.entity.UserMasterEntity;
 import com.heroku.form.LoginForm;
 import com.heroku.mapper.UserMasterMapper;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Controller
 public class UserVaildContreller {
 
-    @Autowired
-    private UserMasterMapper userMapper;
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
 
-    @RequestMapping("/login")
+    @RequestMapping("login")
+    String login() {
+        return "login";
+    }
+
+    /**
+     * ajax登录请求
+     * @param loginForm
+     * @return
+     */
+    @RequestMapping(value="ajaxLogin",method= RequestMethod.POST)
     @ResponseBody
-    Map<String, Object> login(@RequestBody LoginForm form) {
+    public Map<String,Object> submitLogin(@RequestBody LoginForm loginForm) {
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
 
-        System.out.println(form.getInputEmail());
+/*        if(vcode==null||vcode==""){
+            resultMap.put("status", 500);
+            resultMap.put("message", "验证码不能为空！");
+            return resultMap;
+        }*/
 
-        System.out.println(form.getInputPassword());
+/*        Session session = SecurityUtils.getSubject().getSession();
+        //转化成小写字母
+        vcode = Svcode.toLowerCase();
+        String v = (String) session.getAttribute("_code");
+        //还可以读取一次后把验证码清空，这样每次登录都必须获取验证码
+        //session.removeAttribute("_come");
+        if(!vcode.equals(v)){
+            resultMap.put("status", 500);
+            resultMap.put("message", "验证码错误！");
+            return resultMap;
+        }*/
 
-        Map<String, Object> result = new HashMap();
+        try {
+            UsernamePasswordToken token = new UsernamePasswordToken(loginForm.getUsername(), loginForm.getPassword(),loginForm.getRememberMe());
+            SecurityUtils.getSubject().login(token);
+            resultMap.put("status", 200);
+            resultMap.put("message", "登录成功");
 
-
-        return result;
-//        try (Connection connection = dataSource.getConnection()) {
-//            Statement stmt = connection.createStatement();
-//
-//            StringBuilder sb = new StringBuilder();
-//            sb.append(" SELECT ");
-//            sb.append(" * ");
-//            sb.append(" FROM ");
-//            sb.append(" user_master ");
-//            sb.append(" WHERE  ");
-//            sb.append(" username = '" + form.getInputEmail() + "'");
-//            sb.append(" AND passwd = '" + form.getInputPassword() + "'");
-//            ResultSet rs = stmt.executeQuery(sb.toString());
-//
-//            ArrayList<String> output = new ArrayList<String>();
-//            if (rs.next()) {
-//                //
-//                result.put("flag", "OK");
-//            } else {
-//                result.put("flag", "NG");
-//            }
-//            return result;
-//        } catch (Exception e) {
-//            result.put("flag", "ERR");
-//            return result;
-//        }
+        } catch (Exception e) {
+            resultMap.put("status", 500);
+            resultMap.put("message", e.getMessage());
+        }
+        return resultMap;
     }
 
 
